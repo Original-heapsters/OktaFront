3278549f5fa4c03184105f3217560a4c451f3d29
Russell Tan <russell.tan@zyamusic.com>
2018-04-28 18:39:51 -0700

    Added base files for backend integration

---
+++ CachAR/CachAR/BackendInterface/CacheBack.swift
@@ -0,0 +1,36 @@
+//
+//  CacheBack.swift
+//  CachAR
+//
+//  Created by Russell Tan on 4/28/18.
+//  Copyright © 2018 Andrew Briare. All rights reserved.
+//
+
+import Foundation
+
+class CacheBack {
+    
+    func postUser(){
+        
+    }
+    
+    func getUser(){
+        
+    }
+    
+    func placeAsset(){
+        
+    }
+    
+    func getNearbyAssets(){
+        
+    }
+    
+    func getAsset(){
+        
+    }
+    
+    func markAsset(){
+        
+    }
+}
