933126d78884643c82d8f824f542a7e2ce5e6d34
Russell Tan <russell.tan@zyamusic.com>
2018-04-28 19:20:42 -0700

    Hooked up an endpoint

--- CachAR/CachAR/BackendInterface/CacheBack.swift
+++ CachAR/CachAR/BackendInterface/CacheBack.swift
@@ -7,11 +7,45 @@
 //
 
 import Foundation
+import Alamofire
 
 class CacheBack {
+    var settings: NSDictionary?
+    func setup() {
+        if let path = Bundle.main.path(forResource: "backendSettings", ofType: "plist") {
+            if let dictRoot = NSDictionary(contentsOfFile: path) {
+                self.settings = dictRoot
+            }
+        }
+    }
 
     func postUser() {
+        guard self.settings != nil else {
+            return
+        }
+        let baseUrl = self.settings!["baseUrl"] as! String
+        let endPoint = self.settings!["userEndpoint"] as! String
+        let requestString = baseUrl + endPoint
 
+        let parameters = [
+            "userId": "jhdsfksdh7429837429",
+            "username": "Paul",
+            "radius": "10"
+        ]
+
+        Alamofire.request(requestString, method: .post, parameters: parameters, encoding: URLEncoding(destination: .queryString)).responseJSON { response in
+            print("RESPONSE \(response.result.value ?? "val")")
+            print("RESPONSE \(response.result)")
+            print("RESPONSE \(response)")
+
+            switch response.result {
+            case .success:
+                print("RESPONSE \(response.result)")
+
+            case .failure(let error):
+                print("RESPONSE \(error)")
+            }
+        }
     }
 
     func getUser() {
--- CachAR/CachAR/ViewControllers/SignInViewController.swift
+++ CachAR/CachAR/ViewControllers/SignInViewController.swift
@@ -10,8 +10,32 @@ import Foundation
 import UIKit
 
 class SignInViewController: UIViewController {
+    let cacheBack = CacheBack()
 
     @IBAction func closeButtonClicked(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func postUserClicked(_ sender: Any) {
+        self.cacheBack.postUser()
+    }
+    @IBAction func getUserClicked(_ sender: Any) {
+        self.cacheBack.getUser()
+    }
+    @IBAction func placeClicked(_ sender: Any) {
+        self.cacheBack.placeAsset()
+    }
+    @IBAction func getNearbyClicked(_ sender: Any) {
+        self.cacheBack.getNearbyAssets()
+    }
+
+    @IBAction func findClicked(_ sender: Any) {
+        self.cacheBack.getAsset()
+    }
+    @IBAction func markClicked(_ sender: Any) {
+        self.cacheBack.markAsset()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.cacheBack.setup()
     }
 }
